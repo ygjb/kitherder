4e55d5515ecf0548d92fef3b9a1c6ddc5519bf79
wiredcrow <ren.sy.c@gmail.com>
2013-05-21 23:11:41 -0700

    first implementation of the project model

--- kitherder/matchmaker/models.py
+++ kitherder/matchmaker/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+	ProjectName = models.CharField(max-length=70, unique=True)
+	ParentProjectID = models.ForeignKey(Project)
+	DivisionID = models.ForeignKey(Division)
+	MentorID = models.ForeignKey(Mentor)
+	MenteeID = models.ForeignKey(Mentee)
+	Approved = models.BooleanField()
+	ApprovedBy = models.ForeignKey(Coordinator)
+	ProjectDescription = models.CharField(max-lenght=300)
+	TermsAgree = models.BooleanField()
+	ProjectStatusID = models.ForeignKey(ProjectStatus)
+	SkillsRequired = models.charField(max-length=300)
+	
+class User(models.Model):
+	UserID = models.IntegerField(unique=True)
+	Email = models.CharField(max-length=50, unique=True)
+	IsVouched = models.BooleanField()
+	IsAdmin = models.BooleanField()
+	
+class Mentee(models.Model):
+	UserID = models.ForeignKey(User, unique=True)
+	
+class Mentor(models.Model):
+	UserID = models.ForeignKey(User, unique=True)
+
+class Coordinator(models.Model):
+	UserID = models.ForeignKey(User, unique=True)
+	
+class RELCoordinatorDivision(models.Model):
+	CoordinatorID = models.ForeignKey(Coordinator)
+	DivisionID = models.ForeignKey(Division)
+	
+class Division(models.Model)
+	DivisionName = models.Charfield(max-length=50, unique=True)
+	
+class ProjectStatus(models.Model)
+	ProjectStatus = models.Charfield(max=length=30, unique=True)
+	Deprecated = models.BooleanField()
+	
+class Milestone(models.Model)
+	ProjectID = models.ForeignKey(Project)
+	MilestoneName = models.CharField(max-length=100)
+	MilestoneStatus = models.CharField(max-length=80)
+	MilestoneComments = models.CharField(max-length=500)
+	StartDate = models.DateTimeField()
+	ProjectedEndDate = models.DateTimeField()
+	CompletionDate = models.DateTimeField()
+	
+	
